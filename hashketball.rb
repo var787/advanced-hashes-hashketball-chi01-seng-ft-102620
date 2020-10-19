f1bf8e8a02eed2752021dedc681d91ddc431c825
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-19 07:42:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -148,7 +148,7 @@ end
 
 def team_colors(team_name)
   game_hash.each do |team,info|
-    if       info[:team_name]==team_name
+    if info[:team_name]==team_name
       return info[:colors]
     end
   end
