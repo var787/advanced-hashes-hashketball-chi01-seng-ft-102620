1faa4b14b129b81e3044a5685f3571abc1b9539c
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-19 03:32:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,7 +128,7 @@ end
 
 def num_points_scored(name)
   game_hash.each do |team,team_values|
-    values.each do |stat_name,stats|
+    team_values.each do |stat_name,stats|
     end
   end
   binding.pry
