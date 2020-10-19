e1262a9eab1a397b06a857edc4bf00c499c3ea82
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-19 04:12:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,7 +128,6 @@ end
 
 def num_points_scored(player_name)
   #binding.pry
-  points=game_hash[:home][:players][:player_name][:points]
-  points
+  game_hash[:home][:players][:player_name][:points]
   binding.pry
 end
