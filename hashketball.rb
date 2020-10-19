95f8096ed4ceb310f125d705985bc6de5e918f51
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-19 07:07:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -180,7 +180,7 @@ def player_numbers(tname)
    pstats={}
   game_hash.collect do |team, info|
     info[:players].collect do |key,value|
-      if n[:player_name] == player_name
+      if value[:player_name] == player_name
         pstats=info[:players]
         pstats
       end
